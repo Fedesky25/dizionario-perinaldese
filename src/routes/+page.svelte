d90ea0cb01ae16bfc10ba167bd618892099734a8
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-07-31 19:24:34 +0200

    feat: test pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
+<script>
+    function logout() {
+        fetch("/logout", {
+            credentials: "include",
+            method: "POST"
+        });
+    }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<a href="/login">Login</a>
+<a href="/admin">admin</a>
+<a href="/admin/dizionario">dizionario</a>
+
+<button on:click={logout}>logout</button>
