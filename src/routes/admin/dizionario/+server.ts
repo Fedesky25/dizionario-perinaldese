2a9c5f683d02206094fb4163d9deeb8b509e8cf1
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-08-01 19:06:05 +0200

    feat: search param

--- src/routes/admin/dizionario/+server.ts
+++ src/routes/admin/dizionario/+server.ts
@@ -1,14 +1,14 @@
 import type { RequestHandler } from "./$types";
 import { words } from "$lib/db";
-import { summary_projection, type Summary } from "$lib/word-utils";
+import { summary_projection, getSearchFilter, type Summary } from "$lib/word-utils";
 import { json } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async ({ url }) => {
-    const page = +(url.searchParams.get("skip") || 0);
-    console.log("received: " + page)
-    const res = await words.find({}, {
-        limit: 50,
-        skip: 50*page,
+    const search = url.searchParams.get("search") || '';
+    const filter = search ? getSearchFilter(search) : {};
+    const skip = Math.abs(Math.round(+(url.searchParams.get("skip") || 0)));
+    const res = await words.find(filter, {
+        limit: 50, skip,
         sort: {_id: -1},
         projection: summary_projection
     }).toArray() as unknown[] as Summary[];
