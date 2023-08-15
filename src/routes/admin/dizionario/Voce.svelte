fd5fb1a63ed8a111ef80b64b954849817331dd3d
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-08-15 15:50:29 +0200

    fix: dizionario voce

--- src/routes/admin/dizionario/Voce.svelte
+++ src/routes/admin/dizionario/Voce.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { fgs, type Summary } from "$lib/word-utils";
+    import type { Summary } from "$lib/words/types";
     export let word: Summary;
     export let index: number;
 </script>
@@ -7,8 +7,8 @@
 <tr>
     <td>{word.parola}{#if word.ordine}<sup>{word.ordine}</sup>{/if}</td>
     <td>{word.traduzione}</td>
-    <td>{fgs[word.funzione]}</td>
-    <td><img src="/icons/{word.descrizione ? 'tick' : 'cross'}.svg" alt={word.descrizione ? 'sì' : 'no'}></td>
+    <td>{word.funzione}</td>
+    <td><img src="/icons/{word.con_descrizione ? 'tick' : 'cross'}.svg" alt={word.con_descrizione ? 'sì' : 'no'}></td>
     <td class="{word.numero_esempi ? 'green' : 'red'}-cell">
         {#if word.numero_esempi}
             {word.numero_esempi}
@@ -17,17 +17,17 @@
         {/if}
     </td>
     <div class="menu">
-        <a href="/admin/dizionario/{word._id}">
+        <a href="/admin/dizionario/{word.id}">
             <img src="/icons/edit.svg" alt="Modifica">
             <span>Modifica</span>
         </a>
-        {#if !word.descrizione}
-            <a href="#descrizione/{word._id}/{index}">
+        {#if !word.con_descrizione}
+            <a href="#descrizione/{word.id}/{index}">
                 <img src="/icons/description.svg" alt="Descrizione">
                 <span>Scrivi desc.</span>
             </a>
         {/if}
-        <a href="#rimuovi/{word._id}/{index}">
+        <a href="#rimuovi/{word.id}/{index}">
             <img src="/icons/delete.svg" alt="Rimuovi">
             <span>Rimuovi</span>
         </a>
@@ -50,8 +50,8 @@
 
         background-color: white;
         padding: .5rem 1.2rem .65rem;
-        border-top-left-radius: var(--medium-radius);
-        border-top-right-radius: var(--medium-radius);
+        border-top-left-radius: .5rem;
+        border-top-right-radius: .5rem;
         box-shadow: 0 0 25px #0002;
         /* clip-path: polygon(-30px .5px, calc(100% + 30px) .5px, calc(100% + 30px) calc(100% + 30px), -30px calc(100% + 30px)); */
         clip-path: polygon(-30px -30px, calc(100% + 30px) -30px, calc(100% + 30px) 100%, -30px 100%);
