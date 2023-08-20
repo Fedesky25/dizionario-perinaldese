6153708929505ef8fa52fcef2768042dbae80935
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-08-20 12:48:51 +0200

    feat: input con radice

---
+++ src/lib/words/InputConRadice.svelte
@@ -0,0 +1,92 @@
+<script context="module">
+	import { browser } from "$app/environment";
+
+	let w1 = 0.190, w2 = 0.494;
+	if(browser) {
+		setTimeout(()=>{
+			let s = document.createElement('span');
+			s.style.position = 'absolute';
+			s.style.fontSize = '1rem';
+			s.style.fontWeight = "500";
+			const rem = parseFloat(getComputedStyle(document.documentElement).fontSize);
+			document.body.appendChild(s);
+			s.textContent = '|';
+			w1 = s.getBoundingClientRect().width / rem - .03;
+			s.textContent = '<';
+			w2 = s.getBoundingClientRect().width / rem - .03;
+			document.body.removeChild(s);
+			console.log(`Widths computed:   | ${w1}em    < ${w2}em`);
+		}, 1000);
+	}
+</script>
+
+<!-- svelte-ignore module-script-reactive-declaration -->
+<script lang="ts">
+    export let
+        radice: string,
+        value: string,
+        name: string,
+		id: string|undefined = undefined,
+        disabled = false,
+		readonly = false;
+	let pre: string;
+    let w: number;
+	$: c = value.charCodeAt(0);
+	$: switch(c) {
+        case 124: 
+            pre = radice;
+            w = w1;
+            break;
+        case 60:
+            pre = radice.slice(0, -1);
+            w = w2;
+            break;
+        default:
+            pre = '';
+            w = 0;
+    }
+</script>
+
+<label class="input-like" style="--w: {w}em;" class:disabled>
+	<span>{pre}</span>
+    <input
+        {name} {id}
+		{disabled}
+		{readonly}
+		bind:value={value}
+		size={value.length + 1}
+		autocapitalize="off">
+</label>
+
+<style>
+	label {
+		width: fit-content;
+		display: flex;
+        align-items: baseline;
+	}
+	label.disabled {cursor: not-allowed;}
+	span {
+		color: #999;
+		margin-right: calc(0px - var(--w));
+		user-select: none;
+	}
+	input {
+		border: 0;
+		border-radius: 0;
+		margin-bottom: 0;
+		background: transparent;
+		font-size: inherit;
+		font-weight: inherit;
+		padding: 0;
+		clip-path: inset(0 0 0 var(--w));
+	}
+	input:focus {
+		outline: none;
+		box-shadow: none;
+	}
+	input:disabled,
+	label.disabled > span {
+		opacity: 0;
+		cursor: not-allowed;
+	}
+</style>
