60514df02e65482ae883e86152cbc2794e295bd9
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-08-26 23:19:09 +0200

    feat: participio style + defaults

--- src/routes/admin/dizionario/[id]/Participio.svelte
+++ src/routes/admin/dizionario/[id]/Participio.svelte
@@ -2,6 +2,11 @@
 	import InputConRadice from "$lib/words/InputConRadice.svelte";
 	import { participi_default } from "$lib/words/coniugazione";
 	import type { StrictDeclinazione } from "$lib/words/types";
+	import { formatta, eq } from "$lib/words/utils";
+
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher<{parola: string}>();
 
     export let single: boolean;
     export let radice: string;
@@ -9,6 +14,7 @@
     export let data: StrictDeclinazione|null;
 
     $: default_values = participi_default[numero];
+    $: default_values && update_defaults();
     
     let ms: string, mp: string, fs: string, fp: string;
     $: if(data) {
@@ -19,15 +25,18 @@
     } else set_defaults();
     
     function set_defaults() {
-        ms = default_values[0];
-        mp = default_values[1];
-        fs = default_values[2];
-        fp = default_values[3];
+        ms = '|' + default_values[0];
+        mp = '|' + default_values[1];
+        fs = '|' + default_values[2];
+        fp = '|' + default_values[3];
     }
+    function update_defaults() { if(in_default) set_defaults(); }
 
-    $: in_default = ms === default_values[0] && (
+    $: dispatch("parola", formatta(radice, ms||fs||mp||fp));
+
+    $: in_default = eq(ms,default_values[0]) && (
         single || (
-            mp === default_values[1] && fs === default_values[2] && fp === default_values[3]
+            eq(mp,default_values[1]) && eq(fs,default_values[2]) && eq(fp,default_values[3])
         )
     );
 
@@ -41,7 +50,7 @@
 <svelte:body on:keyup={handleDefault} />
 
 {#if single}
-    <div class="oneline">
+    <div class="oneline" class:default={in_default}>
         <h3>Participio passato</h3>
         <InputConRadice name="coniugazione.participio.ms" bind:value={ms} {radice} />
     </div>
@@ -50,7 +59,7 @@
     <input type="hidden" name="coniugazione.participio.fp" value="NULL">
 {:else}
     <h3>Participio passato</h3>
-    <div class="grid">
+    <div class="grid" class:default={in_default}>
         <label for="part-pass-ms">masch sing</label>
         <InputConRadice {radice} name="coniugazione.participio.ms" bind:value={ms} id="part-pass-ms"/>
         <label for="part-pass-mp">masch plur</label>
@@ -64,5 +73,39 @@
 <input type="hidden" disabled={!in_default} name="coniugazione.participio" value="NULL">
 
 <style>
-
+    h3 {
+        text-align: center;
+    }
+    .grid {
+        display: grid;
+        row-gap: 1rem;
+        column-gap: 1ch;
+        width: fit-content;
+        margin: .5rem auto;
+    }
+    @media (max-width: 80ch) {
+        .grid {grid-template-columns: 2fr 3fr;}
+    }
+    @media (min-width: 80ch) {
+        .grid {grid-template-columns: repeat(4, 1fr);}
+    }
+    label {
+        text-align: right;
+        align-self: center;
+        white-space: nowrap;
+        cursor: text;
+    }
+    .oneline {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        column-gap: 1ch;
+    }
+    .oneline h3 {
+        justify-self: end;
+        font-size: 1rem;
+        font-weight: 400;
+    }
+    .default {
+        --input-color: burlywood;
+    }
 </style>
