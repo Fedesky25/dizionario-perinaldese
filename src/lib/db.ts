6d699a40578402c8033123f05bd9188e7e62fe4b
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-08-28 13:23:29 +0200

    feat: convert postgrest error

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,7 @@
 import { MongoClient } from "mongodb";
-import { createClient } from "@supabase/supabase-js";
+import { createClient, type PostgrestError} from "@supabase/supabase-js";
 import { DB_URI, SUPABASE_URL, SUPABASE_KEY } from "$env/static/private";
+import { error } from "@sveltejs/kit";
 
 const client = new MongoClient(DB_URI);
 await client.connect();
@@ -11,3 +12,11 @@ export const proverbs = database.collection("proverbs");
 export const proverbtags = database.collection("proverbtags");
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export const postgresError2HTTPError = (err: PostgrestError) => (
+    console.log(err),
+    error(500, {
+        message: err.message,
+        details: err.details
+    })
+);
