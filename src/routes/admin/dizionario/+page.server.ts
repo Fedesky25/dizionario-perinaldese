5a7a2d2743d955e9e2bc0c652c127d35faa1e460
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-08-28 15:56:08 +0200

    refactor: use supabase in dizionario actions

--- src/routes/admin/dizionario/+page.server.ts
+++ src/routes/admin/dizionario/+page.server.ts
@@ -1,12 +1,12 @@
 import type { Actions, PageServerLoad } from "./$types";
-import { postgresError2HTTPError, supabase } from "$lib/db";
+import { postgresError2HTTPError } from "$lib/db";
 import type { Summary } from "$lib/words/types";
 import { error } from "@sveltejs/kit";
 import { getInt, getMandatoryString } from "$lib/form-utils";
 
 
-export const load: PageServerLoad = async () => {
-    const res = await supabase.rpc("riassunto");
+export const load: PageServerLoad = async ({ locals }) => {
+    const res = await locals.supabase.rpc("riassunto");
     if(res.error) throw error(500, {
         message: res.error.message,
         details: res.error.details
@@ -16,18 +16,18 @@ export const load: PageServerLoad = async () => {
 }
 
 export const actions: Actions = {
-    descrizione: async ({ request }) => {
+    descrizione: async ({ request, locals }) => {
         const data = await request.formData();
         const descrizione = getMandatoryString(data, "descrizione");
         const id = getInt(data, "id");
-        const res = await supabase.from("parole").update({descrizione}).eq("id",id);
+        const res = await locals.supabase.from("parole").update({descrizione}).eq("id",id);
         if(res.error) throw postgresError2HTTPError(res.error);
         return { success: true }
     },
-    rimuovi: async ({ request }) => {
+    rimuovi: async ({ request, locals }) => {
         const data = await request.formData();
         const id = getInt(data, "id");
-        const res = await supabase.from("parole").delete().eq("id",id);
+        const res = await locals.supabase.from("parole").delete().eq("id",id);
         if(res.error) throw postgresError2HTTPError(res.error);
         return { success: true }
     }
