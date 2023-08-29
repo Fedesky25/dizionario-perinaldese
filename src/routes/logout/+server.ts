2e1ccc6e17d4777de9f584b5a456180f05533602
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-08-29 17:43:45 +0200

    chore: removed unused logout endpoint

--- src/routes/logout/+server.ts
+++
@@ -1,8 +0,0 @@
-import { removeAuthorization } from '$lib/cookies';
-import type { RequestHandler } from './$types';
-import { redirect } from "@sveltejs/kit";
-
-export const POST: RequestHandler = async ({ cookies }) => {
-    removeAuthorization(cookies);
-    throw redirect(302, "/");
-};
