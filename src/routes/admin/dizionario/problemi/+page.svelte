47a005d93a3fbbb3eb8b2d81bdb090213e2920ae
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-09-04 17:30:51 +0200

    refactor: issues multiple

--- src/routes/admin/dizionario/problemi/+page.svelte
+++ src/routes/admin/dizionario/problemi/+page.svelte
@@ -2,32 +2,43 @@
 	import type { PageData } from "./$types";
     import SortableList from "./SortableList.svelte";
     import Modal from "$lib/ConfirmModalForm.svelte";
-	import type { SubmitFunction } from "@sveltejs/kit";
-	import { getIntList, InvalidField } from "$lib/form-utils";
+	import { clientFormHandler, getInt, getIntList, InvalidField } from "$lib/form-utils";
 	import { enhance } from "$app/forms";
     import Loading from "$lib/Loading.svelte";
 
     export let data: PageData;
 
-    let err_msg = '';
     let remove: Modal;
     let removing: {id: number, parola: string, traduzione: string}|null = null;
-    let submitting = false;
 
-    const submit: SubmitFunction = async ({ formData, cancel }) => {
-        cancel();
-        if(submitting) return;
-        submitting = true;
-        try {
-            const ids = getIntList(formData, "id");
-            const res = await data.supabase.from("parole").update({ordine: 0}).in("id", ids);
-            if(res.error) err_msg = res.error.details;
-            else data.single = [];
-        }
-        catch(err) {
-            err_msg = err instanceof InvalidField ? `${err.field}: ${err.expected}` : err+'';
-        }
-        submitting = false;
+    const single = clientFormHandler(async (formData) => {
+        const ids = getIntList(formData, "id");
+        const res = await data.supabase.from("parole").update({ordine: 0}).in("id", ids);
+        if(res.error) throw res.error.details;
+        else data.single = [];
+    });
+
+    let considered = -1;
+    const multiple = clientFormHandler(async (formData: FormData) => {
+        considered = getInt(formData, "index");
+        const id_parole = getIntList(formData, "id");
+        const res = await data.supabase.rpc("aggiorna_ordini", {id_parole});
+        if(res.error) throw res.error.details;
+        data.multiple.splice(considered, 1);
+        data.multiple = data.multiple;
+        considered = -1;
+    });
+
+    function onsingle(e: CustomEvent<{extra: number, id: number, traduzione: string}>) {
+        const i = e.detail.extra;
+        data.single.push({
+            id: e.detail.id,
+            parola: data.multiple[i].parola,
+            traduzione: e.detail.traduzione
+        });
+        data.multiple.splice(i,1);
+        data.single = data.single;
+        data.multiple = data.multiple;
     }
 </script>
 
@@ -41,21 +52,43 @@
         <h2>Ordine delle definizioni multiple</h2>
         <p>Queste parole hanno più traduzioni con errori nel loro ordine: alcuni hanno lo stesso valore di ordine o hanno valore di ordine nullo.</p>
         <div class="grid">
-            {#each data.multiple as word, index}
+            {#each data.multiple as word, index (word.parola)}
+                {@const formID = `multipla-${word.parola}`}
                 <div class="parola">{word.parola}</div>
-                <SortableList items={word.significati} extra={index} onremove={(id, traduzione) => {
-                    removing = { id, traduzione, parola: word.parola };
-                    return remove.confirm();
-                }} />
+                <form id={formID} action="?/multipla" method="post" use:enhance={multiple.submit}>
+                    <input type="hidden" name="index" value={index}>
+                    <SortableList 
+                        extra={index} 
+                        items={word.significati} 
+                        onremove={(id, traduzione) => {
+                            removing = { id, traduzione, parola: word.parola };
+                            return remove.confirm();
+                        }}
+                        on:single={onsingle} />
+                </form>
+                <div class="last">
+                    {#if index === considered}
+                    <Loading />
+                    {:else}
+                    <button type="submit" form={formID} disabled={$multiple.submitting}>Invia</button>
+                    {/if}
+                </div>
                 <hr>
             {/each}
         </div>
+        {#if $multiple.error}
+            <p class="err">{
+                $multiple.error instanceof InvalidField
+                ? `${$multiple.error.field}: ${$multiple.error.expected}`
+                : $multiple.error
+            }</p>
+        {/if}
     </section>
     {#if data.single.length > 0}
     <section>
         <h2>Definizione singola</h2>
         <p>Le seguenti parole hanno una sola definizione, tuttavia riportano un ordine diverso da 0. Clicca sul tasto risolvi per sistemare automaticamente questo problema.</p>
-        <form action="?/singolo" method="post" use:enhance={submit}>
+        <form action="?/singolo" method="post" use:enhance={single.submit}>
             <ul>
                 {#each data.single as s}
                     <li>
@@ -64,14 +97,14 @@
                     </li>
                 {/each}
             </ul>
-            {#if submitting}
+            {#if $single.submitting}
             <Loading />
             {:else}
-            <button type="submit">Risolvi</button>
+            <button class="center" type="submit">Risolvi</button>
             {/if}
         </form>
-        {#if err_msg}
-        <p class="err">{err_msg}</p>
+        {#if $single.error}
+        <p class="err">{$single.error instanceof InvalidField ? `${$single.error.field}: ${$single.error.expected}` : $single.error}</p>
         {/if}
     </section>
     {/if}
@@ -105,18 +138,21 @@
     }
     .grid {
         display: grid;
-        grid-template-columns: auto 1fr;
+        grid-template-columns: auto 1fr auto;
         column-gap: 2rem;
         max-width: fit-content;
         margin: 1.5rem auto;
+        align-items: center;
     }
     .parola {
         justify-self: right;
-        align-self: center;
         padding-left: 1rem;
     }
+    .last {
+        padding-right: 1rem;
+    }
     hr {
-        grid-column: 1/3;
+        grid-column: 1/4;
         height: 0;
         border: none;
         border-bottom: 1px solid #ccc;
@@ -144,13 +180,19 @@
         color: white;
         padding: .4rem .8rem;
         border-radius: .15rem;
-        margin: 0 auto;
         display: block;
         cursor: pointer;
+    }
+    button.center {
+        margin: 0 auto;
     }
     a {
         color: inherit;
         text-decoration: underline dashed var(--olivina);
+    }
+    button:disabled {
+        opacity: 0.8;
+        filter: grayscale(0.9);
     }
     form {
         --loading-width: 20ch;
