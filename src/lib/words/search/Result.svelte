72f45803759be1ba18a92abfde86dcf7caec4af5
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-09-08 12:51:11 +0200

    feat: new word search

---
+++ src/lib/words/search/Result.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    export let data: {parola: string, funzione: string, traduzione: string};
+</script>
+
+<div>
+    <span class="lex">{data.parola}</span>
+    <span class="fgs">{data.funzione}</span>
+    <span class="tra">{data.traduzione}</span>
+</div>
+
+<style>
+    div {
+        display: grid;
+        grid-template-columns: auto auto;
+        column-gap: 3ch;
+    }
+    .lex {
+        font-weight: 700;
+        grid-row: 1;
+        grid-column: 1;
+        justify-self: start;
+    }
+    .fgs {
+        color: lightslategray;
+        grid-row: 1;
+        grid-column: 2;
+        justify-self: end;
+    }
+    .tra {
+        grid-row: 2;
+        grid-column: 1/3;
+        justify-self: start;
+    }
+    .tra::before {
+        content: ">> ";
+        color: #ccc;
+    }
+</style>
---
+++ src/lib/words/search/index.ts
@@ -0,0 +1,22 @@
+import type { SupabaseClient } from "@supabase/supabase-js";
+import type { Database } from "$lib/supabase";
+import Result  from "./Result.svelte";
+import { getSearchFilter } from "../utils";
+
+interface SearchResult<S extends boolean> {
+    traduzione: string;
+    funzione: string;
+    parola: string;
+    id: number | (S extends true ? never : null);
+}
+
+function getSearchAction<S extends boolean>(client: SupabaseClient<Database>, singola: S) {
+    const procedure = (singola ? "ricerca_singola" as const : "ricerca_completa" as const);
+    return async function (text: string): Promise<SearchResult<S>[]>  {
+        const res = await client.rpc(procedure, getSearchFilter(text));
+        if(res.error) throw res.error;
+        return res.data;
+    }
+}
+
+export { Result, getSearchAction }
