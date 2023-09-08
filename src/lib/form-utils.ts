b4a066e2db751d2815b493582e6cf0b5ec0eddfa
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-09-08 12:50:03 +0200

    refactor: form-utils postgresError wrapper

--- src/lib/form-utils.ts
+++ src/lib/form-utils.ts
@@ -1,3 +1,4 @@
+import type { PostgrestError, PostgrestSingleResponse } from "@supabase/supabase-js";
 import { error, type SubmitFunction } from "@sveltejs/kit";
 import { writable, type Readable } from "svelte/store";
 
@@ -20,13 +21,34 @@ export class InvalidField extends Error {
     }
 }
 
+export class PostgrestErrorWrapper implements PostgrestError {
+    public readonly message: string;
+    public readonly details: string;
+    public readonly code: string;
+    public readonly hint: string;
+
+    constructor(err: PostgrestError) {
+        this.message = err.message;
+        this.details = err.details;
+        this.code = err.code;
+        this.hint = err.hint;
+    }
+    toString() {
+        return `${this.message}: ${this.details} -> ${this.hint}`;
+    }
+}
+
+export function getDataOrThrow<T>(res: PostgrestSingleResponse<T>): T {
+    if(res.error) throw new PostgrestErrorWrapper(res.error);
+    return res.data;
+}
+
 export function datoInvalido(msg: string): never {
     throw error(400, { message: "Dato non valido", details: msg });
 }
 
 
-
-type FormError = InvalidField|string|null;
+type FormError = InvalidField|PostgrestErrorWrapper|string|null;
 interface FormStoreData<T> {
     submitting: boolean;
     error: FormError;
@@ -37,10 +59,11 @@ interface FormStore<T> extends Readable<FormStoreData<T>> {
     get value(): FormStoreData<T>;
     reset(): void;
 }
+export type ClientFormHandlerCallback<T> = (arg: {formData: FormData, element: HTMLFormElement, who: T}) => Promise<void>
 
 export function clientFormHandler<T = null>(
     get_who: ((arg: {formData: FormData, element: HTMLFormElement}) => T)|null,
-    fn: (arg: {formData: FormData, element: HTMLFormElement, who: T}) => Promise<void>,
+    fn: ClientFormHandlerCallback<T>
 ): FormStore<T>
 {
     let submitting = false;
@@ -59,7 +82,10 @@ export function clientFormHandler<T = null>(
             error = null;
         }
         catch(err) {
-            if(err instanceof InvalidField) error = err;
+            if(
+                err instanceof InvalidField ||
+                err instanceof PostgrestErrorWrapper
+            ) error = err;
             else error = err + '';
         }
         submitting = false;
@@ -111,4 +137,17 @@ export function getIntList(data: FormData, key: string): number[] {
             `${Number.isNaN(v) ? "stringa" : "numero decimale e/o negativo"} al ${i+1}-esimo elemento`
         );
     });
+}
+
+export function getStringList(data: FormData, key: string): string[] {
+    const res = data.getAll(key);
+    for(var i=0; i<res.length; i++) {
+        const v = res[i];
+        const s = typeof v === "string";
+        if(!s || v.length === 0) throw new InvalidField(key, 
+            "Lista stringhe", 
+            `${s ? "stringa vuota" : v === null ? "niente" : "file"} al ${i+1}-esimo elemento`
+        );
+    }
+    return res as string[];
 }
