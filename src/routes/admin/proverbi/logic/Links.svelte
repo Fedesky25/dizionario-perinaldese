296fb11065fd9fb2cf6efa7bdf54150c927970b5
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-09-09 13:04:32 +0200

    refactor: proverbi links better navigation

--- src/routes/admin/proverbi/logic/Links.svelte
+++ src/routes/admin/proverbi/logic/Links.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import Search from "$lib/Search.svelte";
-    import { getSearchAction, Result } from "$lib/words/search";
+    import { getSearchAction, Result, type SearchResult } from "$lib/words/search";
 	import type { SupabaseClient } from "@supabase/supabase-js";
 	import type { Database } from "$lib/supabase";
 	import { wordLinks } from "./logic";
@@ -21,9 +21,77 @@
         //@ts-ignore
         xshift = this.offsetParent.offsetLeft + 0.5 * this.clientWidth;
     }
+
+    function followConsidered() {
+        const l = document.querySelector(`label[for="add-link"][data-index="${considered}"]`);
+        //@ts-ignore
+        if(l) xshift = l.offsetParent.offsetLeft + 0.5 * l.clientWidth;
+        else considered = -1;
+    }
+
+    function onselect(this: Search<SearchResult<true>>, e: CustomEvent<SearchResult<true>>) {
+        const d = e.detail;
+        links.link(considered, d.id, d.parola);
+        this.clear();
+        if($links.length === considered+1) {
+            considered = -1;
+            this.blur();
+        }
+        else {
+            considered++;
+            followConsidered();
+        }
+    }
+
+    function move(e: KeyboardEvent) {
+        if(e.key !== "Tab") return;
+        if(e.shiftKey) {
+            considered--;
+            if(considered < 0) return;
+            e.preventDefault();
+            followConsidered();
+        }
+        else {
+            if(considered === $links.length-1) considered = -1;
+            else {
+                e.preventDefault();
+                considered++;
+                followConsidered();
+            }
+        }
+    }
+
+    function tabFromOut(this: HTMLInputElement, e: FocusEvent) {
+        if(considered >= 0) return;
+        const other = e.relatedTarget;
+        if(other instanceof HTMLElement) {
+            const res = this.compareDocumentPosition(other);
+            switch(res) {
+                case Node.DOCUMENT_POSITION_PRECEDING:
+                    e.preventDefault();
+                    considered = 0;
+                    followConsidered();
+                    break;
+                case Node.DOCUMENT_POSITION_FOLLOWING:
+                    e.preventDefault();
+                    considered = $links.length-1;
+                    followConsidered();
+                    break;
+                default:
+                    console.log("Unknown focus origin");
+            }
+        }
+    }
+
+    let container: HTMLDivElement
+    function onClickOut(e: Event) {
+        if(e.target instanceof Node && !container.contains(e.target)) considered = -1;
+    }
 </script>
 
-<div>
+<svelte:document on:click={onClickOut} on:contextmenu={onClickOut} />
+
+<div bind:this={container}>
     <ul>
         {#each $links as l, index}
         <li class:linked={l.id}>
@@ -38,17 +106,15 @@
         </li>
         {/each}
     </ul>
-    <label for="add-link" class="search" class:show={considered !== -1} style:--x="{xshift}px">
+    <label for="add-link" class="search" class:show={considered >= 0} style:--x="{xshift}px">
         <Search 
             id="add-link"
-            autoclear 
             action={getSearchAction(client, true)}
             placeholder="link a {$links[considered]?.piece}"
-            on:select={e => {
-                const d = e.detail;
-                links.link(considered, d.id, d.parola);
-                considered = -1;
-            }}>
+            on:keydown={move}
+            on:select={onselect}
+            on:focus={tabFromOut}
+            >
                 <p slot="empty">Cerca la voce del vocabolario corrispondente a &laquo;{$links[considered]?.piece}&raquo;</p>
                 <Result slot="result" let:data {data} />
         </Search>
@@ -102,18 +168,28 @@
         --scale: 0;
         position: absolute;
         z-index: 10;
-        left: var(--x);
+        left: 0;
         top: 100%;
-        transform: translate(-50%, .5rem) scale(var(--scale));
+        transform: translate(calc(var(--x) - 50%), 1rem) scale(var(--scale));
+        transition: transform 120ms ease;
         padding: .4rem .8rem;
         background-color: white;
         border-radius: .3rem;
-        box-shadow: 0 0 .7rem #3335;
+        box-shadow: 0 0 1rem #3333;
+    }
+    .search::before {
+        content: '';
+        position: absolute;
+        left: 50%;
+        top: 1px;
+        transform: translate(-50%, -100%);
+        border: 8px solid transparent;
+        border-bottom-color: white;
     }
     .search p {
         width: 25ch;
     }
-    .search.show:focus-within {
+    .search.show {
         --scale: 1;
     }
 </style>
