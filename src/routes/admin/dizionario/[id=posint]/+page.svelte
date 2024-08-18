7fbc97a5ba2fe6baa636ff598b8d392657ca47d4
Fedesky25 <fedesky.guglielmi@gmail.com>
2024-08-18 13:56:16 +0200

    feat(dizionario): editor form handler callback

--- src/routes/admin/dizionario/[id=posint]/+page.svelte
+++ src/routes/admin/dizionario/[id=posint]/+page.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
 	import type { PageData } from "./$types";
     import Editor from "$lib/words/editor/Editor.svelte"
+    import { type ClientFormHandlerCallback } from "$lib/form-utils";
+    import { getDataFromForm, updateWord, updateCollegamenti } from "$lib/words/editor/logic";
+	import { goto } from "$app/navigation";
 
     export let data: PageData;
+
+    const handler: ClientFormHandlerCallback<null> = async ({formData}) => {
+        const body = getDataFromForm(formData, data.funzioni.map(v => v.id)); 
+        await updateWord(data.id, body, data.supabase);
+        await updateCollegamenti(data.id, formData, data.supabase);
+        goto("/admin/dizionario");
+    }
 </script>
 
-<Editor client={data.supabase} fgs={data.funzioni} id={data.id} word={data.parola} />
+<Editor client={data.supabase} fgs={data.funzioni} id={data.id} word={data.parola} {handler} />
